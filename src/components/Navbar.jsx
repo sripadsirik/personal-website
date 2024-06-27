d2a483ebe680763fd1d1a13ebd2e3422330e6ae9
sripadsirik <sripadsirik@gmail.com>
2024-06-27 09:10:28 -0500

    nav initial

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,31 @@
-import React, { useEffect, useState} from 'react';
-import { Link } from 'react-router-dom';
-import { styles } from '../styles';
-import { navLinks } from '../constants';
-import { logo, menu, close } from '../assets';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
   return (
-    <div>Navbar</div>
+    <nav
+    className={`${
+      styles.paddingX
+    } w-full flex items-center py-5 bg-primary`}
+    >
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+        </Link>
+      </div>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
