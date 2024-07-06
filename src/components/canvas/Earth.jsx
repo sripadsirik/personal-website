d641a777b200b5c8c8af4b64c1f2904216236d49
sripadsirik <sripadsirik@gmail.com>
2024-07-06 16:23:36 -0500

    done with models

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -4,38 +4,50 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  const earth = useGLTF('./planet/scene.gltf');
+  const earth = useGLTF('./mclaren_senna/scene.gltf');
 
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <mesh>
+      <hemisphereLight intensity={10.15} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={3} />
+
+      <primitive 
+        object={earth.scene}
+        scale={1}
+        position={[0, -1.25, 0]}
+        rotation={[-0.01, -.2, -0.1]}
+      />
+    </mesh>
   );
 };
 
 const EarthCanvas = () => {
   return (
     <Canvas
+      frameLoop="demand"
       shadows
-      frameloop='demand'
-      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
-      camera={{
-        fov: 45,
-        near: .1,
-        far: 200,
-        position: [-4, 3, 6]
-      }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
+        <OrbitControls 
           autoRotate
-          enableZoom={false}
+          enableZoom={false} 
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
         <Earth />
-
-        <Preload all />
       </Suspense>
+
+      <Preload all />
     </Canvas>
   );
 };
