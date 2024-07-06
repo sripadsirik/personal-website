1d38782d75946ffdff328de60592f15d423027d9
sripadsirik <sripadsirik@gmail.com>
2024-07-06 02:18:03 -0500

    loader done, new 3d models

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,12 +4,12 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+const Computers = () => {
+  const computer = useGLTF("./gaming_setup_v2_low-poly/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={4.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -18,61 +18,85 @@ const Computers = ({ isMobile }) => {
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
-      <primitive
+      <pointLight intensity={2} />
+
+      <primitive 
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={1.3}
+        position={[0, -6.25, 0]}
+        rotation={[-0.01, -.2, -0.1]}
       />
     </mesh>
   );
 };
 
 const ComputersCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
-    setIsMobile(mediaQuery.matches);
-
-    // Define a callback function to handle changes to the media query
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-    };
-
-    // Add the callback function as a listener for changes to the media query
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
-  }, []);
-
-  return (
+  return(
     <Canvas
-      frameloop='demand'
+      frameLoop="demand"
       shadows
-      dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
+        <OrbitControls 
+          enableZoom={false} 
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={isMobile} />
+        <Computers />
       </Suspense>
 
       <Preload all />
     </Canvas>
-  );
-};
+  )
+}
+
+// const ComputersCanvas = () => {
+//   const [isMobile, setIsMobile] = useState(false);
+
+//   useEffect(() => {
+//     // Add a listener for changes to the screen size
+//     const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+//     // Set the initial value of the `isMobile` state variable
+//     setIsMobile(mediaQuery.matches);
+
+//     // Define a callback function to handle changes to the media query
+//     const handleMediaQueryChange = (event) => {
+//       setIsMobile(event.matches);
+//     };
+
+//     // Add the callback function as a listener for changes to the media query
+//     mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+//     // Remove the listener when the component is unmounted
+//     return () => {
+//       mediaQuery.removeEventListener("change", handleMediaQueryChange);
+//     };
+//   }, []);
+
+//   return (
+//     <Canvas
+//       frameloop='demand'
+//       shadows
+//       dpr={[1, 2]}
+//       camera={{ position: [20, 3, 5], fov: 25 }}
+//       gl={{ preserveDrawingBuffer: true }}
+//     >
+//       <Suspense fallback={<CanvasLoader />}>
+//         <OrbitControls
+//           enableZoom={false}
+//           maxPolarAngle={Math.PI / 2}
+//           minPolarAngle={Math.PI / 2}
+//         />
+//         <Computers isMobile={isMobile} />
+//       </Suspense>
+
+//       <Preload all />
+//     </Canvas>
+//   );
+// };
 
 export default ComputersCanvas;
+
