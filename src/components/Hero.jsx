78af934aac33744dc286c34ecd5ee151ac251539
Sri Sirikonda <ssiri5@uic.edu>
2024-07-30 22:01:10 -0500

    s

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,8 +41,8 @@ const Hero = () => {
           </div>
         </div>
       </div>
-
-      <ComputersCanvas />
+{/* 
+      <ComputersCanvas /> */}
 
       <div className='absolute xs:bottom-25 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
